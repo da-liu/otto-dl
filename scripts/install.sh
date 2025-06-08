71efc773979543d3219ca8e9e2dbea591e4baafb
Da Liu <dluoft@gmail.com>
2025-06-08 11:09:52 -0400

    new change

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Get the latest release version
-LATEST_RELEASE=$(curl -s https://api.github.com/repos/YOUR_USERNAME/otto-dl/releases/latest | grep "tag_name" | cut -d '"' -f 4)
+LATEST_RELEASE=$(curl -s https://api.github.com/repos/da-liu/otto-dl/releases/latest | grep "tag_name" | cut -d '"' -f 4)
 
 # Download the VSIX file
-curl -L -o otto-dl.vsix "https://github.com/YOUR_USERNAME/otto-dl/releases/download/${LATEST_RELEASE}/otto-dl-${LATEST_RELEASE}.vsix"
+curl -L -o otto-dl.vsix "https://github.com/da-liu/otto-dl/releases/download/${LATEST_RELEASE}/otto-dl-${LATEST_RELEASE}.vsix"
 
 # Install the extension
 code --install-extension otto-dl.vsix
